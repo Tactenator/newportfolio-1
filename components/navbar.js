d1c7476c0a06c97fc01655a5c7cc4e8daffdb8e7
Trevor McLaurine <trevor.mclaurine@gmail.com>
2022-11-06 11:04:10 -0600

    Welcome Page Test Mobile

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Navbar = () => {
     return ( 
         <div>
-            <nav class="flex bg-[#0a192f] justify-end text-slate-200 p-6 text-2xl gap-11 fixed mx-auto inset-x-0">
+            <nav id="desktop-nav" class="hidden bg-[#0a192f] justify-end text-slate-200 p-6 text-2xl gap-11 fixed mx-auto inset-x-0 sm:flex">
                 <Link href="#welcome"><a>Welcome</a></Link>
                 <Link href="#about"><a>About</a></Link>
                 <Link href="#projects"><a>Projects</a></Link>
--- pages/welcome.js
+++ pages/welcome.js
@@ -7,10 +7,10 @@ const Welcome = () => {
                 <title>Trevor McLaurine</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
-            <div class="flex flex-col justify-start">
-                <p class="text-4xl text-slate-200">Hello, my name is</p>
-                <h1 class="pt-4 pb-4 text-6xl"><strong class="text-teal-300">Trevor McLaurine</strong> </h1>
-                <p class="text-5xl text-slate-200">A Web Developer</p>
+            <div class="flex flex-col text-center sm:text-left">
+                <p class="text-3xl text-slate-200">Hello, my name is</p>
+                <h1 class="pt-4 pb-4 text-5xl"><strong class="text-teal-300">Trevor McLaurine</strong> </h1>
+                <p class="text-4xl text-slate-200">A Web Developer</p>
             </div>
         </div>
      );
