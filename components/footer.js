82087663ad604bcf4ce672366c80718ab04b9773
Trevor McLaurine <trevor.mclaurine@gmail.com>
2022-11-06 21:53:36 -0600

    Final mobile touches

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-        <div className="bg-[#0a192f] text-teal-400 pt-96 text-center p-2">
+        <div className="bg-[#0a192f] text-teal-400 pt-96 text-center sm:p-2">
             &copy;Trevor McLaurine 2022 Made with <strong className="text-slate-200">NextJS</strong> and <strong className="text-slate-200">Tailwind CSS</strong>
         </div>
      );
